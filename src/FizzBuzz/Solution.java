4b9045f501646cc52efff5af21b9b4458a563612
Leonardo <leonardojpy@gmail.com>
2025-04-02 13:45:10 -0300

    First commit

---
+++ src/FizzBuzz/Solution.java
@@ -0,0 +1,27 @@
+package FizzBuzz;
+
+//LeetCode description: https://leetcode.com/problems/fizz-buzz/description/
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public List<String> fizzBuzz(int n) {
+        List<String> answer = new ArrayList<>();
+        int contador = 0;
+        while (contador < n) {
+            contador++;
+            if (contador % 3 == 0 && contador % 5 == 0){
+                answer.add("FizzBuzz");
+            } else if (contador % 5 == 0){
+                answer.add("Buzz");
+            } else if (contador % 3 == 0){
+                answer.add("Fizz");
+            } else {
+                answer.add(String.valueOf(contador));
+            }
+        }
+        return answer;
+    }
+}
+
