5fb4e17df7a2734d46529643b12618e1fdaabfc3
Leonardo <leonardojpy@gmail.com>
2025-04-03 16:21:02 -0300

    Adding new exercise.

---
+++ src/PalindromeNumber/Solution.java
@@ -0,0 +1,28 @@
+package PalindromeNumber;
+
+//LeetCode description: https://leetcode.com/problems/palindrome-number/description/
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+
+    public boolean isPalindrome(int x) {
+        List<Character> index = new ArrayList<>();
+        String numero = String.valueOf(x);
+        for (int i = 0; i < numero.length(); i++){
+            index.add((numero.charAt(i)));
+        }
+        int i = 0;
+        while (i < index.size()){
+            if (index.getFirst() == index.getLast()){
+                index.removeFirst();
+                index.removeLast();
+                i++;
+            } else {
+                return false;
+            }
+        }
+    return true;
+    }
+}
