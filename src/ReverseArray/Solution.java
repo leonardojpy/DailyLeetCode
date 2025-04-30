3dcf08428652e4d530b66c105c2a8e0c20ec641d
Leonardo <leonardojpy@gmail.com>
2025-04-30 16:21:09 -0300

    Reversing array positions

---
+++ src/ReverseArray/Solution.java
@@ -0,0 +1,17 @@
+package ReverseArray;
+//This exercise is not on LeetCode.
+public class Solution {
+    public static int[] inverterArray(int[] numeros){
+        int left = 0;
+        int right = numeros.length - 1;
+
+        while (left < right){
+            int temp = numeros[right];
+            numeros[right] = numeros[left];
+            numeros[left] = temp;
+            left++;
+            right--;
+        }
+        return numeros;
+    }
+}
